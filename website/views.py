0fed9929e5d3ebaeab32c7422762003fc16c328d
Ayush Jain <ayushjaink8@gmail.com>
2021-06-10 04:22:32 +0530

    moving ahead with django

--- website/views.py
+++ website/views.py
@@ -3,5 +3,15 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+# def home(request):
+#     return HttpResponse("Hello World");
+
 def home(request):
-    return HttpResponse("Hello World");
+    return render(request, "test.html",{'name':'Naveen'});
+
+def test2(request):
+
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    val3 = val1 + val2
+    return render(request, "test2.html", {'result': val3} );
