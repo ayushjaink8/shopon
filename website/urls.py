36092cee1fe7f59ed54e6e24fa70cd9e32a7ce11
Ayush Jain <ayushjaink8@gmail.com>
2021-06-11 13:57:28 +0530

    added all static pages

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns =[
     path('',views.index, name='index'),
+    path('contact',views.contact, name='contact'),
+    path('about',views.about, name='about'),
+    path('products',views.products, name='products'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 
-
 # Create your views here.
-
 
 # def home(request):
 #     return HttpResponse("Hello World");
 
 def index(request):
     return render(request, "index.html");
+
+def about(request):
+    return render(request, "about.html");
+
+def contact(request):
+    return render(request, "contact.html");
+
+
+def products(request):
+    return render(request, "products.html");
+
+
+
+
+
 
 # def test2(request):
 #     val1 = int(request.POST['num1'])
